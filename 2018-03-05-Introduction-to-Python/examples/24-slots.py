6addfce84312ef6ea07cdfc71777d4a77d26880d
Petr Zemek <s3rvac@gmail.com>
2018-03-06 09:10:24 +0100

    2018-03-05: Improve a comment in 24-slots.py.

--- 2018-03-05-Introduction-to-Python/examples/24-slots.py
+++ 2018-03-05-Introduction-to-Python/examples/24-slots.py
@@ -15,6 +15,7 @@ class B:
 a = A(1, 2)
 assert '__dict__' in dir(a)
 
-# 'b' will have a lower memory footprint than 'a' due to the use of __slots__:
+# 'b' will have a lower memory footprint and provide faster attribute access
+# than 'a' due to the use of __slots__:
 b = B(1, 2)
 assert '__dict__' not in dir(b)
